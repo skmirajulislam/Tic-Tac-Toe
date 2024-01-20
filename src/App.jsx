81a6933302281ca8d9cd5fac6c80c6d05443c94a
SK MIRAJUL ISLAM <122096119+skmirajulislam@users.noreply.github.com>
2024-01-20 15:35:47 +0530

    by sk

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import  Tictactoe  from "./Components/Tictactoe";
+function App() {
+  return (
+    <>
+      <div className="app">
+       <Tictactoe></Tictactoe>
+      </div>
+    </>
+  )
+}
+
+export default App
